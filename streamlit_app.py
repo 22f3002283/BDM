44e897732daf27410502e468936a2de984d33e12
22f3002283 <22f3002283@ds.study.iitm.ac.in>
2025-11-30 05:38:11 +0000

    form

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Sales forecastig")
-x=st.selectbox("What do ou like",["Cat","Dog"])
+month=st.selectbox("Select month",["festival","Non Festival"])
+if "non" in month:month="November"
+else:month="October"
 st.write(
-   str(x)
+   str(month)
 )
+items=pd.read_csv("items.csv")
+product=st.selectbox("Product Name ",items["Product"])
